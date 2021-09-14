95b163d63fd57735e08f0b124eac4308bc36ff56
Stéphane THOMAS <sthomas@campus-eni.fr>
2021-09-14 11:54:14 +0200

    - Centrage formulaire de login - Ajout images - Ajout champ Description et lienWiki dans entité Animal

---
+++ migrations/Version20210913131415.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210913131415 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE animal ADD description LONGTEXT DEFAULT NULL, ADD lien_wiki VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE animal DROP description, DROP lien_wiki');
+    }
+}
