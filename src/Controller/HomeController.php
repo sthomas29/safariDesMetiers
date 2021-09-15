81957424cee39006d5be35286c02e7f4c0052c79
Stéphane THOMAS <sthomas@campus-eni.fr>
2021-09-15 23:09:04 +0200

    Ajustement CSS page principale + Création card avec contenu

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,21 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\AnimalRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $em;
+    private $repo;
+
+    public function __construct(EntityManagerInterface $entityManager, AnimalRepository $repo)
+    {
+        $this->em = $entityManager;
+        $this->repo = $repo;
+
+    }
+
     #[Route('/safari', name: 'home')]
     public function index(): Response
     {
-
+        $animaux = $this->repo->findAll();
 
 
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'animaux' => $animaux,
         ]);
     }
 }
